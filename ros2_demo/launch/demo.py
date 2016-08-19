39b2210b6c10b98f6e98c1d611e1d8be9b9f2bfb
Jon Binney <jon.binney@gmail.com>
2016-08-19 16:57:46 -0700

    Ros2 version of apriltags

--- ros2_demo/launch/demo.py
+++ ros2_demo/launch/demo.py
@@ -8,11 +8,16 @@ def launch(launch_descriptor, argv):
         name='driver_node',
         exit_handler=restart_exit_handler,
     )
-    # ld.add_process(
-    #     cmd=['astra_camera_node__rmw_opensplice_cpp'],
-    #     name='astra_camera_node',
-    #     exit_handler=restart_exit_handler,
-    # )
+    ld.add_process(
+        cmd=['astra_camera_node__rmw_opensplice_cpp'],
+        name='astra_camera_node',
+        exit_handler=restart_exit_handler,
+    )
+    #ld.add_process(
+    #    cmd=['apriltag_detector_node__rmw_opensplice_cpp'],
+    #    name='apriltag_detector_node',
+    #    exit_handler=restart_exit_handler,
+    #)
     ld.add_process(
         cmd=['please_move'],
         name='please_mode_node',
@@ -21,5 +26,10 @@ def launch(launch_descriptor, argv):
     ld.add_process(
         cmd=['rosdi_ws'],
         name='rosdi_ws',
+        exit_handler=restart_exit_handler,
+    )
+    ld.add_process(
+        cmd=['follower__rmw_opensplice_cpp'],
+        name='follower',
         exit_handler=restart_exit_handler,
     )
